b8df1e323ebffed90eb88115355fffb7ec76d577
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-18 10:21:31 +0200

    Test init

---
+++ src/test/java/com/example/productrepository/controller/ProductControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.productrepository.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+}
